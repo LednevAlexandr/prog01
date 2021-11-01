7e26bb407954e6e86481e4e6dcafda9f0da429bd
alex <you@example.com>
2021-11-01 22:21:16 +0300

    added prog01

---
+++ prog01/Program.cs
@@ -0,0 +1,35 @@
+﻿int GetRandom(int minValue, int maxValue)
+{
+    return new Random().Next(minValue,maxValue);
+}
+
+int[] A = new int[20];
+
+int index=0;
+        while(index<20)
+        {
+            A[index] = GetRandom(10,100);
+            index++;
+        }
+
+index = 0;
+        while(index<20)
+        {
+            Console.WriteLine(A[index] +" ");
+            index++;
+        }
+
+Console.WriteLine();
+index = 1;
+int firstelement = A[0];
+Console.WriteLine(A[0]);
+while(index<10)
+{
+   if(A[index]>firstelement)
+      {
+          Console.WriteLine(A[index]);
+           firstelement = A[index];    
+      }
+    
+    index++;
+}
