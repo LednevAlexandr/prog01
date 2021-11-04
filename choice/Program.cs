377d945de16ef1680378a380f7bb4e3b10a963b7
alex <you@example.com>
2021-11-04 12:03:20 +0300

    исправленный алгоритм

--- choice/Program.cs
+++ choice/Program.cs
@@ -19,7 +19,7 @@ void SelectionSort(int[] array)
     for (int i = 0; i < array.Length-1; i++)
     {
         int minPosition = i;
-        for (int j = i+1; j < array.Length; i++)
+        for (int j = i+1; j < array.Length; j++)
         {
             if(array[j] < array[minPosition]) minPosition = j;
         }
